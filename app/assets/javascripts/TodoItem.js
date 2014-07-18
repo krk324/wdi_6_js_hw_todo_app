e83ecdc087a897f0ad41b56ed2c5a4da928d38ef
krk324 <krk32488@gmail.com>
2014-07-17 21:43:11 -0400

    App now add delete and done button to each task.

--- app/assets/javascripts/TodoItem.js
+++ app/assets/javascripts/TodoItem.js
@@ -1,5 +1,7 @@
 var TodoItem = function(task){
     if(task !== ''){
-      return $('<li>').text(task);
+      var deleteButton = '<button type="button" class="btn btn-danger btn-sm">Delete</button>';
+      var doneButton = '<button type="button" class="btn btn-success btn-sm">Done</button>';
+      return $('<li>').text(task).append(doneButton,deleteButton);
     }
   };
