a10df9e4c2e31eee52d4faf6309117f30421fed5
krk324 <krk32488@gmail.com>
2014-07-17 20:04:22 -0400

    App can now add task to the to-do list.

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,14 @@
 var TodoApp = {
-
+  initialize: function(){
+    $('#task-form' ).submit(this.addTask);
+  },
+  addTask: function(event){
+    var task = $('#task-field').val();
+    if(task !== ''){
+      var newTask = $('<li>').text(task);
+      $('#tasks-list').append(newTask);
+      $('#task-field').val('');
+    }
+    event.preventDefault();
+  }
 };
