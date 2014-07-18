10151986aa1a281c3bcf06a99a047d1ab7b9ea71
krk324 <krk32488@gmail.com>
2014-07-18 11:36:26 -0400

    App now lists finished task on the done list.

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -2,6 +2,8 @@ var TodoApp = {
   initialize: function(){
     $('#task-form').submit(this.createTask);
     $('#tasks-list').on('click', '#delete-button',this.deleteTask);
+    $('#tasks-list').on('click', '#done-button',this.doneTask);
+    $('#finished-list').on('click', '#delete-button',this.deleteTask);
   },
   createTask: function(event){
     var task = $('#task-field').val();
@@ -18,6 +20,11 @@ var TodoApp = {
   },
   deleteTask: function(event){
     $( this.parentElement ).remove();
+    event.preventDefault();
+  },
+  doneTask: function(event){
+    var finished_task = $( this.parentElement ).detach();
+    $('#finished-list').append(finished_task);
     event.preventDefault();
   }
 };
