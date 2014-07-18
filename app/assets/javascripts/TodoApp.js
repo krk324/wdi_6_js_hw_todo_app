2829048150731eb9f55636a2730601904bf53dc5
krk324 <krk32488@gmail.com>
2014-07-18 19:43:22 -0400

    Add filter dropdown.

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -4,17 +4,13 @@ var TodoApp = {
     $('#tasks-list').on('click', '#delete-button',this.deleteTask);
     $('#tasks-list').on('click', '#done-button',this.doneTask);
     $('#finished-list').on('click', '#delete-button',this.deleteTask);
+
   },
   createTask: function(event){
     var task = $('#task-field').val();
     if(task !== ''){
       var newTask = TodoItem(task);
       $('#tasks-list').append(newTask);
-
-      i = $( "#tasks-list li" ).index($('#tasks-list li').last());
-
-      $( "#tasks-list li" ).last().attr('data-id',i);
-
       $('#task-field').val('');
     }
     event.preventDefault();
@@ -29,5 +25,8 @@ var TodoApp = {
     finished_task.attr('completed-at',TodoItem.prototype.createdAt);
     $('#finished-list').append(finished_task);
     event.preventDefault();
-  }
+  },
+  sortAlphabatical: function(event){
+    $('#task-menu li').each();
+    }
 };
