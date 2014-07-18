ef602d3bd1d0a456054e314c40449357fc1200f5
krk324 <krk32488@gmail.com>
2014-07-18 14:54:51 -0400

    App now add createdate on li tag.

--- app/assets/javascripts/TodoItem.js
+++ app/assets/javascripts/TodoItem.js
@@ -2,6 +2,16 @@ var TodoItem = function(task){
     if(task !== ''){
       var deleteButton = '<button type="button" class="btn btn-danger btn-sm" id ="delete-button">Delete</button>';
       var doneButton = '<button type="button" class="btn btn-success btn-sm" id = "done-button">Done</button>';
-      return $('<li>').text(task).append(doneButton,deleteButton);
+      var newtask = $('<li>').text(task).append(doneButton,deleteButton).attr('created-at',TodoItem.prototype.createdAt);
+      return newtask;
     }
   };
+
+TodoItem.prototype = {
+  createdAt: function(){
+  return Date();
+  },
+  completedAt: function(){
+  return Date();
+  }
+};
